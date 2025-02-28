ef3b01452eef8180b737164573b8aa13be21c3cb
Vishal Das <vishal.das@mkcorp.com>
2025-02-28 08:14:37 +0530

    init-4

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./profile-react-app",
+  base: "/profile-react-app",
   plugins: [react(), tailwindcss()],
 })
